7b11b91cb0d690ad03eea0549a96c07fc094e583
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-10-16 22:04:09 +0200

    Draft for project data structure

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const projects = [
+  {
+    title: 'Habit Tracker',
+    description: 'Habit tracking website built with React ',
+    image: 'ht.png',
+    tools: ['React', 'JavaScript', 'SCSS'],
+    links: {
+      gitHub: 'https://github.com/iwarra/habit-tracker',
+      liveDemo: ''
+    }
+  },
+  {
+    title: 'Vue Dashboard',
+    description: 'User system developed with Vue3 and Vite. State managed with Pinia.',
+    image: 'vue.png',
+    tools: ['Vue3', 'Vite', 'Pinia', 'JavaScript'],
+    links: {
+      gitHub: 'https://github.com/iwarra/vue-system',
+      liveDemo: 'https://vue-system-dashboard.netlify.app/'
+    }
+  },
+  {
+    title: 'Quiz App',
+    description: 'A quiz developed using Typescript and React. Styled with styled components. ',
+    image: '',
+    tools: ['React', 'TypeScript', 'Style Components'],
+    links: {
+      gitHub: 'https://github.com/iwarra/quiz-app',
+      liveDemo: 'https://fun-trivia-night.netlify.app/'
+    }
+  },
+  {
+    title: 'Podcast App',
+    description: 'Habit tracking website built with React ',
+    image: '',
+    tools: ['HTML', 'JavaScript', 'SCSS'],
+    links: {
+      gitHub: 'https://github.com/iwarra/habit-tracker',
+      liveDemo: ''
+    }
+  },
+]
