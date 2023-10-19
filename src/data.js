239e237b935055cd67b0b681a35634494764477a
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-10-19 15:48:31 +0200

    More project data added

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: 'Habit Tracker',
-    description: 'Habit tracking website built with React ',
+    description: 'Habit tracking website built with React. Under construction.',
     image: 'ht.png',
     tools: ['React', 'JavaScript', 'SCSS'],
     links: {
@@ -13,17 +13,17 @@ export const projects = [
     title: 'Vue Dashboard',
     description: 'User system developed with Vue3 and Vite. State managed with Pinia.',
     image: 'vue.png',
-    tools: ['Vue3', 'Vite', 'Pinia', 'JavaScript'],
+    tools: ['Vue', 'Vite', 'Pinia', 'JavaScript'],
     links: {
-      gitHub: 'https://github.com/iwarra/vue-system',
+      gitHub: 'https://github.com/iwarra/vue-system', 
       liveDemo: 'https://vue-system-dashboard.netlify.app/'
     }
   },
   {
     title: 'Quiz App',
-    description: 'A quiz developed using Typescript and React. Styled with styled components. ',
+    description: 'A quiz app built with Typescript and React. Styled with styled components.',
     image: '',
-    tools: ['React', 'TypeScript', 'Style Components'],
+    tools: ['React', 'TypeScript', 'Styled Components'],
     links: {
       gitHub: 'https://github.com/iwarra/quiz-app',
       liveDemo: 'https://fun-trivia-night.netlify.app/'
@@ -31,12 +31,12 @@ export const projects = [
   },
   {
     title: 'Podcast App',
-    description: 'Habit tracking website built with React ',
-    image: '',
-    tools: ['HTML', 'JavaScript', 'SCSS'],
+    description: 'Test project built with Vanilla JS, HTLM and CSS. The task was to fetch and display 10 podcasts from Sveriges Radio API. ',
+    image: 'pod1.png',
+    tools: ['HTML', 'JavaScript', 'CSS'],
     links: {
-      gitHub: 'https://github.com/iwarra/habit-tracker',
-      liveDemo: ''
+      gitHub: 'https://github.com/iwarra/podcast-project',
+      liveDemo: 'https://podcasts-api.netlify.app/'
     }
   },
 ]
