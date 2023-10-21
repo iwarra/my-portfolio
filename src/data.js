701e799a7bcba511f93d7a525d800b911ba55bf1
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-10-21 20:42:23 +0200

    Added skills

--- src/data.js
+++ src/data.js
@@ -39,4 +39,46 @@ export const projects = [
       liveDemo: 'https://podcasts-api.netlify.app/'
     }
   },
+];
+
+export const skills = [
+  { name: "JavaScript",
+    icon: "fa-brands fa-js"
+  },
+  { name: "HTLM5",
+    icon: "fa-brands fa-html5"
+  },
+  { name: "React",
+    icon: "fa-brands fa-react"
+  },
+  { name: "Vue",
+    icon: "fa-brands fa-vuejs"
+  },
+  { name: "Node.js",
+    icon: "fa-brands fa-node"
+  },
+  { name: "CSS",
+    icon: "fa-brands fa-css3-alt"
+  }, 
+  { name: "SCSS",
+    icon: "fa-brands fa-sass"
+  },
+  { name: "Github",
+    icon: "fa-brands fa-github"
+  },
+  { name: "VS Code",
+    icon: "fa-solid fa-code"
+  },
+  { name: "npm",
+    icon: "fa-brands fa-npm"
+  },
+  { name: "Bootstrap",
+    icon: "fa-brands fa-bootstrap"
+  },
+   { name: "Wordpress",
+    icon: "fa-brands fa-wordpress"
+  },
+  //  { name: "Typescript",
+  //   icon: ""
+  // },
 ]
