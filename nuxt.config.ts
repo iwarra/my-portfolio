e7e9de27339d69fcfe37c02ee68aa644ecf778eb
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-12-25 21:33:16 +0100

    Config and packages updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,51 +1,64 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      title: "Ivona Josipovic | Frontend developer",
-      link: [{
-        rel: "icon",
-        href: "/icon.svg",
-        type: "image/svg+xml"
-      }],
-      meta: [
-        {
-          name: "description",
-          content: "Ivona Josipovic is a frontend developer based in Stockholm. She enjoys building things using HTML, CSS, JavaScript, Vue and React."
-        },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { charset: 'utf-8' },
-      ]
-    },
-    rootId: "portfolio",
-  },
-  pages: true,
-  devtools: { enabled: false },
-  srcDir: "src/",
-    components: {
-    global: true,
-    dirs: ["~/components"]
-  },
-  modules: [
-    "@nuxt/content",
-    "vue3-carousel-nuxt",
-  ],
-  carousel: {
-    prefix: "External"
-  },
-  content: {
-    documentDriven: false,
-    highlight: {
-      theme: 'min-dark',
-      preload: ['vue', 'json', 'jsx', 'sass', 'scss']
-    }
-  },
-  vite: {
-    build: {
-      rollupOptions: {
-        external: ['illustration.svg', 'arrow-left.svg', 'arrow-right.svg', 'quote-left.png', 'quote-right.png'],
-      },
-    }
-  },
-  css: ["~/global.scss"],
-})
+	site: {
+		url: "https://ivona.se",
+		name: "Ivona Josipovic | Frontend developer",
+	},
+	sitemap: {
+		sources: ["ivona.se/api/_sitemap/urls"],
+	},
+	app: {
+		head: {
+			link: [
+				{
+					rel: "icon",
+					href: "/icon.svg",
+					type: "image/svg+xml",
+				},
+			],
+			meta: [
+				{ name: "author", content: "Ivona Josipovic" },
+				{ name: "viewport", content: "width=device-width, initial-scale=1" },
+				{ charset: "utf-8" },
+			],
+		},
+		rootId: "portfolio",
+	},
+	pages: true,
+	devtools: { enabled: false },
+	srcDir: "src/",
+	components: {
+		global: true,
+		dirs: ["~/components"],
+	},
+	modules: [
+		"@nuxt/content",
+		"vue3-carousel-nuxt",
+		"nuxt-simple-sitemap",
+		"@nuxtjs/robots",
+	],
+	carousel: {
+		prefix: "External",
+	},
+	content: {
+		documentDriven: false,
+		highlight: {
+			theme: "min-dark",
+			preload: ["vue", "json", "jsx", "sass", "scss"],
+		},
+	},
+	vite: {
+		build: {
+			rollupOptions: {
+				external: [
+					"illustration.svg",
+					"arrow-left.svg",
+					"arrow-right.svg",
+					"quote-left.png",
+					"quote-right.png",
+				],
+			},
+		},
+	},
+	css: ["~/global.scss"],
+});
