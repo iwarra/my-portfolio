6f24a81e2c8b58133cad2a7c5939db715a48b3d3
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-01-02 20:35:44 +0100

    Switch to carousel component with useScroll

---
+++ src/components/Carousel.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { useScroll } from "@vueuse/core";
+
+const emit = defineEmits(["arrowClicked"]);
+const props = defineProps(["selector"]);
+const ul = ref(null);
+const { arrivedState } = useScroll(ul);
+const { left, right } = toRefs(arrivedState);
+</script>
+
+<template>
+	<div>
+		<ClientOnly>
+			<img
+				src="/arrow-left.svg"
+				alt="arrow pointing left"
+				role="button"
+				:aria-disabled="left"
+				aria-label="previous"
+				class="arrow-icon"
+				@click="$emit('arrowClicked', 'left', ul)" />
+		</ClientOnly>
+		<ul
+			:class="selector"
+			ref="ul">
+			<slot></slot>
+		</ul>
+		<ClientOnly>
+			<img
+				src="/arrow-right.svg"
+				alt="arrow pointing right"
+				role="button"
+				:aria-disabled="right"
+				aria-label="next"
+				class="arrow-icon"
+				@click="$emit('arrowClicked', 'right', ul)" />
+		</ClientOnly>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.arrow-icon[aria-disabled="true"] {
+	filter: grayscale(80%);
+	opacity: 65%;
+	cursor: default;
+}
+
+.slider {
+	list-style: none;
+	display: flex;
+	flex-direction: row;
+	gap: 1rem;
+	flex-wrap: nowrap;
+	overflow: auto;
+	scroll-behavior: smooth;
+	scroll-snap-type: x mandatory;
+	scrollbar-width: none;
+	-webkit-overflow-scrolling: touch;
+	-ms-overflow-style: -ms-autohiding-scrollbar;
+}
+</style>
