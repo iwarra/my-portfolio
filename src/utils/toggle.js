eef5b57aa2c18aab60160e08f0575d1c1967908c
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-01-03 22:17:23 +0100

    Updated with aria-expanded

--- src/utils/toggle.js
+++ src/utils/toggle.js
@@ -1,11 +1,19 @@
 export const toggle = (color) => {
-  document.querySelector(".header-toggle").classList.toggle("header--active") 
+	document.querySelector(".header-toggle").classList.toggle("header--active");
 
-  document.querySelector('.header-navWrapper').classList.toggle(color) 
+	document.querySelector(".header-navWrapper").classList.toggle(color);
 
-  document.querySelector('.header-wrap').classList.toggle(color)
+	document.querySelector(".header-wrap").classList.toggle(color);
 
-  document.getElementById('navTransitionEl').classList.toggle('hidden')
+	document.getElementById("navTransitionEl").classList.toggle("hidden");
 
-  document.querySelector('.header-nav').classList.toggle('hidden')
+	document.querySelector(".header-nav").classList.toggle("hidden");
+
+	document.querySelector(".lang-wrapper").classList.toggle("hidden");
+
+	let navBtn = document.querySelector(".header-trigger")
+	navBtn.setAttribute(
+		"aria-expanded",
+		navBtn.getAttribute("aria-expanded") == "true" ? "false" : "true",
+	);
 };
