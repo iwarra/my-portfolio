7a5ff4c5d8451092e36633d09d105aef7d09ad8c
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-09-01 18:56:44 +0200

    Added testimonial from S

--- src/server/api/testimonials.get.js
+++ src/server/api/testimonials.get.js
@@ -1,19 +1,27 @@
 export default defineEventHandler(() => {
-  return [
-    {
-      from: 'Erik Morlin',
-      workTitle: 'Frontend Developer @Sublime',
-      recommendation: "Ivona faced challenges with a positive attitude and determination to understand. She really got why accessibility and web standards matter, which is crucial in our work.Ivona didn't just learn the theory; she put it into practice, paying close attention to details in her tasks. Her enthusiasm for her work and willingness to learn, combined with her great personality, makes Ivona a valuable team member. \n\n I'm pleased to recommend Ivona as a promising professional who's great to work with. Her learning attitude and frontend skills make her an asset to any organization. She would be a wonderful addition to any team."
-    },
-    {
-      from: 'Stefan Bergfeldt',
-      workTitle: 'System Architect @Sogeti',
-      recommendation: "I've had the opportunity to witness Ivona's remarkable growth and dedication firsthand.Her technical prowess as a front-end developer is truly impressive for someone at that stage in their career. Her ability to swiftly grasp complex concepts and implement them with precision showcases her commitment to excellence. \n\n What truly sets Ivona apart is her remarkable self-awareness. She's not only cognizant of her strengths, but also actively seeks to address any areas where she can improve, demonstrating a genuine passion for continuous learning and growth. Her approachability and open communication style make her incredibly easy to collaborate with, fostering an environment of effective teamwork and synergy. I wholeheartedly recommend Ivona and am excited to see her future accomplishments."
-    },
-    // {
-    //   from: 'Test',
-    //   workTitle: 'Job title',
-    //   recommendation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    // }
-  ]
-})
+	return [
+		{
+			from: 'Erik Morlin',
+			workTitle: 'Frontend Developer @Sublime',
+			recommendation:
+				"Ivona faced challenges with a positive attitude and determination to understand. She really got why accessibility and web standards matter, which is crucial in our work.Ivona didn't just learn the theory; she put it into practice, paying close attention to details in her tasks. Her enthusiasm for her work and willingness to learn, combined with her great personality, makes Ivona a valuable team member. \n\n I'm pleased to recommend Ivona as a promising professional who's great to work with. Her learning attitude and frontend skills make her an asset to any organization. She would be a wonderful addition to any team.",
+		},
+		{
+			from: 'Stefan Bergfeldt',
+			workTitle: 'System Architect @Sogeti',
+			recommendation:
+				"I've had the opportunity to witness Ivona's remarkable growth and dedication firsthand.Her technical prowess as a front-end developer is truly impressive for someone at that stage in their career. Her ability to swiftly grasp complex concepts and implement them with precision showcases her commitment to excellence. \n\n What truly sets Ivona apart is her remarkable self-awareness. She's not only cognizant of her strengths, but also actively seeks to address any areas where she can improve, demonstrating a genuine passion for continuous learning and growth. Her approachability and open communication style make her incredibly easy to collaborate with, fostering an environment of effective teamwork and synergy. I wholeheartedly recommend Ivona and am excited to see her future accomplishments.",
+		},
+		{
+			from: 'Swetha Dua',
+			workTitle: 'Project Manager @Avanade',
+			recommendation:
+				"I was introduced to Ivona through the Pollination mentorship program at Beela during the spring of 2023. As her mentor, I was very proud to see Ivona's determination. Ivona is an excellent communicator and an active listener. During our meetings, she always arrived prepared with an agenda and an action plan to achieve her goals. Ivona took the initiative to lead each of our meetings with clarity and confidence. She demonstrated her interest and commitment through her strong work ethic. In my opinion, adaptability and having an open mind are crucial skills to have in today's rapidly changing technology landscape. Ivona has been very mindful of this from the beginning of her career transition, learning new skills and adapting new technologies to be prepared for any challenges. While I believe Ivona will continue to build her technical expertise, adding new skills to her CV, the soft skills she already has are what sets her apart. If you're looking for new front-end developers — look no further!",
+		},
+		// {
+		//   from: 'Test',
+		//   workTitle: 'Job title',
+		//   recommendation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		// }
+	];
+});
