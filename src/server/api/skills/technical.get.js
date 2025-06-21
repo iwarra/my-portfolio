1cca1e06169ffec8904581bb20fbb2bb177332f0
Ivona Josipovic <josipovic.ivona@gmail.com>
2025-06-21 13:40:03 +0200

    Add docker to skills

--- src/server/api/skills/technical.get.js
+++ src/server/api/skills/technical.get.js
@@ -1,56 +1,67 @@
 export default defineEventHandler(() => {
-  return [
-    { name: "JavaScript",
-      icon: "IconsJs"
-    },
-    { name: "HTLM5",
-      icon: "IconsHtml"
-    },
-    { name: "React",
-      // icon: "fa-brands fa-react",
-      icon: "IconsReact"
-    },
-    { name: "Vue",
-      // icon: "fa-brands fa-vuejs",
-      icon: "IconsVue"
-    },
-    { name: "Node.js",
-      // icon: "fa-brands fa-node",
-      icon: "IconsNode"
-    },
-    { name: "CSS",
-      // icon: "fa-brands fa-css3-alt",
-      icon: "IconsCss"
-    }, 
-    { name: "SCSS",
-      // icon: "fa-brands fa-sass",
-      icon: "IconsScss"
-    },
-    { name: "Github",
-      // icon: "fa-brands fa-github",
-      icon: "IconsGithub"
-    },
-    { name: "VS Code",
-      // icon: "fa-solid fa-code",
-      icon: "IconsVscode"
-    },
-    { name: "npm",
-      // icon: "fa-brands fa-npm",
-      icon: "IconsNpm"
-    },
-    { name: "Bootstrap",
-      // icon: "fa-brands fa-bootstrap",
-      icon: "IconsBootstrap"
-    },
-    { name: "Wordpress",
-      // icon: "fa-brands fa-wordpress",
-      icon: "IconsWordpress"
-    },
-    //  { name: "Typescript",
-    //   icon: ""
-    // },
-     //  { name: "Nuxt",
-    //   icon: ""
-    // },
-  ]
-})
+	return [
+		{ name: 'JavaScript', icon: 'IconsJs' },
+		{ name: 'HTLM5', icon: 'IconsHtml' },
+		{
+			name: 'React',
+			// icon: "fa-brands fa-react",
+			icon: 'IconsReact',
+		},
+		{
+			name: 'Vue',
+			// icon: "fa-brands fa-vuejs",
+			icon: 'IconsVue',
+		},
+		{
+			name: 'Node.js',
+			// icon: "fa-brands fa-node",
+			icon: 'IconsNode',
+		},
+		{
+			name: 'CSS',
+			// icon: "fa-brands fa-css3-alt",
+			icon: 'IconsCss',
+		},
+		{
+			name: 'SCSS',
+			// icon: "fa-brands fa-sass",
+			icon: 'IconsScss',
+		},
+		{
+			name: 'Github',
+			// icon: "fa-brands fa-github",
+			icon: 'IconsGithub',
+		},
+		{
+			name: 'VS Code',
+			// icon: "fa-solid fa-code",
+			icon: 'IconsVscode',
+		},
+		{
+			name: 'npm',
+			// icon: "fa-brands fa-npm",
+			icon: 'IconsNpm',
+		},
+		{
+			name: 'Bootstrap',
+			// icon: "fa-brands fa-bootstrap",
+			icon: 'IconsBootstrap',
+		},
+		{
+			name: 'Wordpress',
+			// icon: "fa-brands fa-wordpress",
+			icon: 'IconsWordpress',
+		},
+		{
+			name: 'Docker',
+			// icon: "fa-brands fa-docker",
+			icon: 'IconsDocker',
+		},
+		//  { name: "Typescript",
+		//   icon: ""
+		// },
+		//  { name: "Nuxt",
+		//   icon: ""
+		// },
+	];
+});
