51cf916ddd10fc12da96438e9644b9ca07b75344
Ivona Josipovic <josipovic.ivona@gmail.com>
2026-01-17 18:37:48 +0100

    Icons fix, order of skills update

--- src/server/api/skills/technical.get.js
+++ src/server/api/skills/technical.get.js
@@ -1,81 +1,77 @@
 export default defineEventHandler(() => {
 	return [
-		{ name: 'JavaScript', icon: 'IconsJs', iconType: 'component'},
 		{ name: 'HTLM5', icon: 'IconsHtml', iconType: 'component' },
 		{
-			name: 'React',
-			// icon: "fa-brands fa-react",
-			icon: 'IconsReact',
-		},
-		{
-			name: 'Vue',
-			// icon: "fa-brands fa-vuejs",
-			icon: 'IconsVue',
-			 iconType: 'component'
-		},
-		{
-			name: 'Node.js',
-			// icon: "fa-brands fa-node",
-			icon: 'IconsNode',
-			 iconType: 'component'
-		},
-		{
 			name: 'CSS',
-			// icon: "fa-brands fa-css3-alt",
 			icon: 'IconsCss',
 			 iconType: 'component'
 		},
-		{
-			name: 'SCSS',
-			// icon: "fa-brands fa-sass",
-			icon: 'IconsScss',
-			 iconType: 'component'
-		},
-		{
-			name: 'Github',
-			// icon: "fa-brands fa-github",
-			icon: 'IconsGithub',
-			 iconType: 'component'
-		},
-		{
-			name: 'VS Code',
-			// icon: "fa-solid fa-code",
-			icon: 'IconsVscode',
-		},
-		{
-			name: 'npm',
-			// icon: "fa-brands fa-npm",
-			icon: 'IconsNpm',
-		},
-		{
-			name: 'Bootstrap',
-			// icon: "fa-brands fa-bootstrap",
-			icon: 'IconsBootstrap',
-			 iconType: 'component'
-		},
-		{
-			name: 'Wordpress',
-			// icon: "fa-brands fa-wordpress",
-			icon: 'IconsWordpress',
-			 iconType: 'component'
-		},
-		{
-			name: 'Docker',
-			// icon: "fa-brands fa-docker",
-			icon: 'IconsDocker',
-			 iconType: 'component'
-		},
-		 { name: "Typescript",
+		{ name: 'JavaScript', icon: 'IconsJs', iconType: 'component'},
+		{ name: "Typescript",
 		  icon: "typescript.svg",
-		 iconType: 'img'
+		  iconType: 'img'
 		},
-		 { name: "Nuxt",
+		{
+			name: 'React',
+			icon: 'IconsReact',
+			iconType: 'component'
+		},
+		{
+			name: 'Vue',
+			icon: 'IconsVue',
+			 iconType: 'component'
+		},
+		{ name: "Nuxt",
 		  icon: "nuxt.svg",
+			iconType: 'img'
+		},
+		{
+			name: 'Node.js',
+			icon: 'IconsNode',
+			 iconType: 'component'
+		},
+		{ name: "C#",
+		  icon: "csharp.svg",
 			iconType: 'img'
 		},
 		 { name: ".NET",
 		  icon: "dotnet.svg",
 			iconType: 'img'
+		},	
+		{
+			name: 'Github',
+			icon: 'IconsGithub',
+			iconType: 'component'
+		},
+		{
+			name: 'VS Code',
+			icon: 'IconsVscode',
+			iconType: 'component'
+		},
+		{
+			name: 'npm',
+			icon: 'IconsNpm',
+			iconType: 'component'
+		},
+		{
+			name: 'SCSS',
+			icon: 'IconsScss',
+			iconType: 'component'
+		},
+		{
+			name: 'Bootstrap',
+			icon: 'IconsBootstrap',
+			iconType: 'component'
+		},
+		{
+			name: 'Wordpress',
+			icon: 'IconsWordpress',
+			iconType: 'component'
+		},
+		{
+			name: 'Docker',
+			icon: 'IconsDocker',
+			iconType: 'component'
 		},
 		 { name: "Supabase",
 		  icon: "supabase.svg",
@@ -84,11 +80,7 @@ export default defineEventHandler(() => {
 		 { name: "Netlify",
 		  icon: "netlify.svg",
 			iconType: 'img'
-		},
-		 { name: "C#",
-		  icon: "csharp.svg",
-			iconType: 'img'
-		},
+		},	 
 		{ name: "Linux",
 		  icon: "linux.svg",
 			iconType: 'img'
@@ -97,16 +89,16 @@ export default defineEventHandler(() => {
 		  icon: "nginx.svg",
 			iconType: 'img'
 		},
+		{ name: "SQL",
+		  icon: "postgresql.svg",
+			iconType: 'img'
+		},
 		{ name: "LangChain",
 		  icon: "langchain.svg",
 			iconType: 'img'
 		},
 		{ name: "MCP",
 		  icon: "mcp.svg",
-			iconType: 'img'
-		},
-		{ name: "SQL",
-		  icon: "postgresql.svg",
 			iconType: 'img'
 		},
 		{ name: "Hugging Face",
